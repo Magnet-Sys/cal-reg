156f2bf6d254f8dfc91626e29728beda20de7ca1
Dianna Monsalve <diannamonsalve@MacBook-Air-de-Dianna.local>
2025-07-19 21:03:04 -0400

    NavBar component is created for good practices

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,12 @@
 <script setup>
-  import { 
-    BNavbar, 
-    BNavbarBrand, 
-    BNavbarToggle, 
-    BCollapse, 
-    BNavbarNav, 
-    BNavItem,
-    BNavForm, 
-    BFormInput, 
-    BButton 
-  } from 'bootstrap-vue-next'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <div>
-    <BNavbar toggleable="lg" variant="light" class="border-bottom px-3">
-      <BNavbarBrand href="#">Programación Front End</BNavbarBrand>
-      
-      <BNavbarToggle target="nav-collapse" />
-
-      <BCollapse id="nav-collapse" is-nav>
-        <BNavbarNav>
-          <router-link to="/" class="nav-link">Cálculo de calificaciones</router-link>
-          <router-link to="/registro" class="nav-link">Formulario de Registro</router-link>
-          <BNavItem href="#" disabled>En construcción</BNavItem>
-        </BNavbarNav>
-        
-        <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-          <BNavForm @submit.prevent>
-            <BFormInput
-              class="me-2"
-              placeholder="Search"
-            />
-            <BButton variant="outline-success" type="submit">Búsqueda</BButton>
-          </BNavForm>
-        </BNavbarNav>
-      </BCollapse>
-    </BNavbar>
-
+    <NavBar />
     <main class="container my-5">
       <router-view />
     </main>
   </div>
 </template>
-
-<style>
-  .router-link-exact-active {
-    font-weight: bold;
-    color: #1a8754 !important;
-  }
-
-  .navbar-brand {
-    color: rgba(0, 0, 0, 0.9) !important;
-  }
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<script setup>
+    import { 
+    BNavbar, 
+    BNavbarBrand, 
+    BNavbarToggle, 
+    BCollapse, 
+    BNavbarNav, 
+    BNavItem,
+    BNavForm, 
+    BFormInput, 
+    BButton 
+    } from 'bootstrap-vue-next'
+</script>
+
+<template>
+  <BNavbar toggleable="lg" variant="light" class="border-bottom px-3">
+    <BNavbarBrand href="#">Programación Front End</BNavbarBrand>
+    
+    <BNavbarToggle target="nav-collapse" />
+
+    <BCollapse id="nav-collapse" is-nav>
+      <BNavbarNav>
+        <router-link to="/" class="nav-link">Cálculo de calificaciones</router-link>
+        <router-link to="/registro" class="nav-link">Formulario de Registro</router-link>
+        <BNavItem href="#" disabled>En construcción</BNavItem>
+      </BNavbarNav>
+      
+      <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+        <BNavForm @submit.prevent>
+          <BFormInput
+            class="me-2"
+            placeholder="Search"
+          />
+          <BButton variant="outline-success" type="submit">Búsqueda</BButton>
+        </BNavForm>
+      </BNavbarNav>
+    </BCollapse>
+  </BNavbar>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+    font-weight: bold;
+    color: #1a8754 !important;
+    }
+
+    .navbar-brand {
+    color: rgba(0, 0, 0, 0.9) !important;
+    }
+</style>
+
